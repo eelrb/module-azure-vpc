780a8d145229a8bf276ff2d77bf05f0e0b1e73d3
eelrb <eelrb@yahoo.com>
2019-05-03 01:14:52 -0700

    add ip output

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,10 @@ output "network_interface_id" {
    value="${azurerm_network_interface.myterraformnic.id}"
 
 }
+
+output "vm_public_ip" {
+
+   value="${azurerm_public_ip.myterraformpublicip.ip_address}"
+
+}
+
