d1141376d1f202c7196a571eee8c3e57fa15cd30
eelrb <eelrb@yahoo.com>
2019-05-03 00:30:17 -0700

    create variable for resource group name

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {}
 
 # Create a resource group if it doesn’t exist
 resource "azurerm_resource_group" "myterraformgroup" {
-    name     = "myResourceGroup"
+    name     = "${resource_group_name}"
     location = "eastus"
 
     tags {
